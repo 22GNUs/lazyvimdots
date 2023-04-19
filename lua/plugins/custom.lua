a290777b26c7b443c5109b85aaeba5860dd06ab8
22GNUs <stormaroon@gmail.com>
2023-04-19 22:38:04 +0800

    Update symbolsoutline lazy

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -2,6 +2,7 @@ return {
   -- add symbols-outline
   {
     "simrat39/symbols-outline.nvim",
+    lazy = true,
     cmd = "SymbolsOutline",
     keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
     config = true,
