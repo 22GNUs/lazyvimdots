b4aa0e241ede92c966db91dfd4d05a5f7bdf0605
22GNUs <stormaroon@gmail.com>
2023-04-20 10:35:16 +0800

    Add chatgpt plugin

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -7,4 +7,29 @@ return {
     keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
     config = true,
   },
+  -- ChatGPT
+  {
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("chatgpt").setup()
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    keys = function()
+      return {
+        {
+          "<leader>ce",
+          function()
+            require("chatgpt").edit_with_instructions()
+          end,
+          desc = "Edit with chatgpt instructions",
+          mode = "v",
+        },
+      }
+    end,
+  },
 }
