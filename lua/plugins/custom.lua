b03728ff34479936f6f872e7c646eba14108c62e
22GNUs <stormaroon@gmail.com>
2023-04-28 21:34:37 +0800

    Add neoscroll

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -33,4 +33,15 @@ return {
       }
     end,
   },
+  -- add neoscroll
+  {
+    "karb94/neoscroll.nvim",
+    lazy = true,
+    enabled = not vim.g.neovide,
+    -- load before BufRead, avoid loading impact on performance
+    event = "VeryLazy",
+    config = function()
+      require("neoscroll").setup()
+    end,
+  },
 }
