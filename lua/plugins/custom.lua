039d13848e59cc7ed947d9334b55abb80267033c
22GNUs <stormaroon@gmail.com>
2024-01-04 00:00:28 +0800

    Remove chatgpt plugin & using extra symbols-outling instead

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -1,38 +1,4 @@
 return {
-  -- add symbols-outline
-  {
-    "simrat39/symbols-outline.nvim",
-    lazy = true,
-    cmd = "SymbolsOutline",
-    keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
-    config = true,
-  },
-  -- ChatGPT
-  {
-    "jackMort/ChatGPT.nvim",
-    lazy = true,
-    cmd = { "ChatGPT", "ChatGPTRun", "ChatGPTActAs", "ChatGPTCompleteCode", "ChatGPTEditWithInstructions" },
-    config = function()
-      require("chatgpt").setup()
-    end,
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-    },
-    keys = function()
-      return {
-        {
-          "<leader>ce",
-          function()
-            require("chatgpt").edit_with_instructions()
-          end,
-          desc = "Edit with chatgpt instructions",
-          mode = "v",
-        },
-      }
-    end,
-  },
   -- add neoscroll
   {
     "karb94/neoscroll.nvim",
