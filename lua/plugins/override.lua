214cd592137da2288fedfcbf726276283a86c695
22GNUs <stormaroon@gmail.com>
2024-01-03 23:56:47 +0800

    Config google-java-format and using aosp style

--- lua/plugins/override.lua
+++ lua/plugins/override.lua
@@ -75,4 +75,17 @@ return {
       end,
     },
   },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        java = { "google-java-format" },
+      },
+      formatters = {
+        ["google-java-format"] = {
+          prepend_args = { "--aosp" },
+        },
+      },
+    },
+  },
 }
