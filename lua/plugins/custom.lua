da2df6da795b0b7a42d4a5f8580a1075773bec8a
22GNUs <stormaroon@gmail.com>
2024-01-04 19:14:53 +0800

    Update zen mode width

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -19,7 +19,7 @@ return {
         function()
           require("zen-mode").toggle({
             window = {
-              width = 0.85, -- width will be 85% of the editor width
+              width = 0.75,
             },
           })
         end,
