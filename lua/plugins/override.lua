4c8e2ca0c6058a1b70a98f96768221154881ef34
22GNUs <stormaroon@gmail.com>
2024-07-01 20:37:39 +0800

    Remove jdtls & add extras

--- lua/plugins/override.lua
+++ lua/plugins/override.lua
@@ -72,47 +72,6 @@ return {
     },
   },
   {
-    "mfussenegger/nvim-jdtls",
-    opts = {
-      jdtls = {
-        handlers = {
-          -- see: https://www.reddit.com/r/neovim/comments/1172p03/jdtls_spams_messages_in_lazyvim/
-          -- disable progress update
-          -- ["language/status"] = function(_, result)
-          -- print(result)
-          -- end,
-          ["$/progress"] = function(_, result, ctx)
-            -- disable progress updates.
-          end,
-        },
-      },
-      root_dir = function(_)
-        -- override the root_dir look up to support multi module project
-        return require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew" })
-      end,
-      full_cmd = function(opts)
-        local fname = vim.api.nvim_buf_get_name(0)
-        local root_dir = opts.root_dir(fname)
-        local project_name = opts.project_name(root_dir)
-        local cmd = vim.deepcopy(opts.cmd)
-        if project_name then
-          vim.list_extend(cmd, {
-            "-configuration",
-            opts.jdtls_config_dir(project_name),
-            "-data",
-            opts.jdtls_workspace_dir(project_name),
-          })
-        end
-
-        -- support lombok
-        local install_path = require("mason-registry").get_package("jdtls"):get_install_path()
-        local jvmArg = "-javaagent:" .. install_path .. "/lombok.jar"
-        table.insert(cmd, "--jvm-arg=" .. jvmArg)
-        return cmd
-      end,
-    },
-  },
-  {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
