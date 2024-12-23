db093c2517373fc4db2d0962f9e5e51c74150e51
22GNUs <stormaroon@gmail.com>
2024-12-23 10:21:07 +0800

    Add supermaven and remove duplicated custom plugins

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -1,32 +1,3 @@
 return {
-  -- add neoscroll
-  {
-    "karb94/neoscroll.nvim",
-    enabled = not vim.g.neovide,
-    -- load before BufRead, avoid loading impact on performance
-    event = "VeryLazy",
-    config = function()
-      require("neoscroll").setup()
-    end,
-  },
-  {
-    "folke/zen-mode.nvim",
-    -- not work in neovide for now
-    enabled = not vim.g.neovide,
-    event = "VeryLazy",
-    keys = {
-      -- Add a <C-n> remap to open neotree
-      {
-        "<leader>uz",
-        function()
-          require("zen-mode").toggle({
-            window = {
-              width = 0.75,
-            },
-          })
-        end,
-        desc = "Toggle Zen Mode",
-      },
-    },
-  },
+  {},
 }
