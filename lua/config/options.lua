01d6e54a733a2cc14dcaac797d1ce38d40377c80
22GNUs <stormaroon@gmail.com>
2024-12-30 09:56:05 +0800

    Update config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,7 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-
 vim.g.transparency = true
 vim.g.tokyonight_dark_float = false
+vim.g.lazyvim_blink_main = false
 require("config.neovide")
